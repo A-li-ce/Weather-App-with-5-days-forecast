f7cbf4385cdb0034ac4e93f6667f30436c23360a
Alice-In-TaeHub <anounymousteddie@gmail.com>
2025-02-01 17:32:07 +0530

    first commit

--- app.js
+++ app.js
@@ -23,8 +23,10 @@ const createWeatherCard = (cityName, weatherItem, index) => {
                 </div>`;
     }else{
         return `<li class="card">
-                <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+        <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+        <h4>${weatherItem.weather[0].description}</h4>
                 <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="weahter-img" >
+                
                 <h4>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)} C</h4>
                 <h4>Wind: ${weatherItem.wind.speed} M/S</h4>
                 <h4>Humidity: ${weatherItem.main.humidity}%</h4>
